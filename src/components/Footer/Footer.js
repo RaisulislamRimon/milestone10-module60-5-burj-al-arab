e27cbce96d3ec5de79d481b1af00b5763ec6f31f
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-19 13:05:42 +0600

    header navbar

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,9 +2,13 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="fixed-bottom my-3">
-      <p>Burj Al Arab</p>
-    </div>
+    <footer className="footer footer-center p-4 mb-3 pt-10  bg-base-300 text-base-content sticky top-[100vh]">
+      <div>
+        <p className="text-lg">
+          Copyright © 2022 - All right reserved by MD. Raisul Islam Rimon
+        </p>
+      </div>
+    </footer>
   );
 };
 
--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -2,9 +2,16 @@ import React from "react";
 
 const PageNotFound = () => {
   return (
-    <div className="">
-      <h1>404 Error</h1>
-      <h1>Page Not Found</h1>
+    <div id="error-page" className="flex justify-center mt-24">
+      <div className="card w-96 bg-base-100 shadow-xl ">
+        <div className="card-body">
+          <h2 className="card-title">Oops!</h2>
+          <p>Sorry, an unexpected error has occurred.</p>
+          <div className="card-actions">
+            <p>This page is not available</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
