82614fbe12c25ce7f3056cac75f21a86eb88f2c0
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-19 10:55:32 +0600

    header & footer setup

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="fixed-bottom my-3">
+      <Card.Footer className="text-muted text-center">Burj Al Arab</Card.Footer>
     </div>
   );
 };
