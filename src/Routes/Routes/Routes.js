61aaadeb1bc491f8067b0596adda1a900c6e5b88
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-19 11:06:36 +0600

    Header added with banner in / route

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
+import Banner from "../../components/Banner/Banner";
 import Main from "../../layout/Main/Main";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Banner />,
+      },
+    ],
   },
 ]);
 
