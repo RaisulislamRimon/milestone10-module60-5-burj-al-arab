a5d91f8f3042575066fc8f55ad4a780ea6724b84
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-19 10:03:23 +0600

    setup

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layout/Main/Main";
+
+const routes    = () =>
+  createBrowserRouter([
+    {
+      path      : "/",
+      element   : <Main />,
+    },
+  ]);
+
+export default routes;
